f940dd850210d01dec3318380b1d37ce02136a37
Salar Hafezi <salar.hfz.work@gmail.com>
2022-11-29 19:27:44 +0330

    add journal to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Header from './components/header';
+import Journal from './components/journal';
+
+import './App.css';
+
 export default function App() {
   return (
-    <div>
-      Hello world!
+    <div className="container">
+      <Header />
+      <Journal />
     </div>
   )
 }
